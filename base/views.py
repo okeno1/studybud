989e25cd603215469eddfc9d719aa42b80e8d3a7
okeno1 <petercoin.1997@gmail.com>
2023-06-04 01:42:21 +0300

    Adding profile page functionality

--- base/views.py
+++ base/views.py
@@ -105,6 +105,13 @@ def room(request,pk):
     context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html',context)
 
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    context = {'user':user}
+    return render(request,'base/profile.html',context)
+
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
