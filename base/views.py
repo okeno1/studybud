184e444262981c5bbb66a0eec91301c1ac97f11d
okeno1 <petercoin.1997@gmail.com>
2023-06-12 00:17:01 +0300

    Adding suggestion to the dropdown and topics section after creating new room

--- base/views.py
+++ base/views.py
@@ -120,16 +120,25 @@ def createRoom(request):
     
     topics = Topic.objects.all()
     if request.method == 'POST':
-        #print(request.POST)
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return  redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)        
+        
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name =  request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user    
+        #     room.save()
+        return  redirect('home')
     
     
-    context = {'form':form,'topics':topics}
+    context = {'form':form,'topics':topics,'topic_name':topic_name}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='login')
